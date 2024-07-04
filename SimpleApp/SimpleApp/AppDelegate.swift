a27bedf2533cb642094c238df2552f00e8f0508b
Loc <loc.nguyen@axpara.com>
2024-07-04 21:33:02 +0700

    feat: add new login button

--- SimpleApp/SimpleApp/AppDelegate.swift
+++ SimpleApp/SimpleApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)        
+         self.window = UIWindow(frame: UIScreen.main.bounds)        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController")
